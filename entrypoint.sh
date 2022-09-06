4bee0323a733c8d978dae53748cef3f7c45855c3
Adama KO <adamako34@hotmail.com>
2022-09-06 09:01:32 +0000

    fix: entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,17 +13,31 @@ else
     exit 1
 fi
 
-env 
+env
 jq . < $EVENT_PATH
 
 # if keyword is found
 if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
 then
     # do something
-    echo "Found keyword."
+    VERSION=$(date +%F.%s)
+
+    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"target_commitish":"master",')"
+    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+
+    if [[ "${LOCAL_TEST}" == *"true"* ]];
+    then
+        echo "## [TESTING] Keyword was found but no release was created."
+    else
+        echo $DATA | http POST $URL | jq .
+    fi
 # otherwise
 else
     # exit gracefully
     echo "Nothing to process."
 fi
-~
